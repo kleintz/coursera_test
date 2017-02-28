c7bfb2424a02591edb52726acffc7b7c58f1070c
m.klets <m.klets@astoundcommerce.com>
2017-02-28 10:07:23 +0200

    advanced task

--- course5_mod1_solution/app.js
+++ course5_mod1_solution/app.js
@@ -9,16 +9,30 @@
 
 
 		$scope.checkLunch=function(){
-		var listItemsStr=$scope.list;
-		if(listItemsStr==""){
-		$scope.recommendMessage="Please enter data first";
-		}else{
+		var listItemsStr = $scope.list;
+		if(listItemsStr==""||listItemsStr==null){
+			$scope.recommendMessageRed="Please enter data first";
+			$scope.recommendMessageGreen=null;
+		}
+		else{
 		var items=listItemsStr.split(",");
 		var count=items.length;
-		if(count<=3){
-		$scope.recommendMessage="Enjoy!";
+		for (var i in items) {			
+			items[i] = items[i].trim();
+			if (items[i]==""){
+				count= count -1;
+			}
+		}
+		if (count == 0) {
+			$scope.recommendMessageRed="Please enter data first";
+			$scope.recommendMessageGreen=null;
+		}
+		else if(count<=3){
+			$scope.recommendMessageGreen="Enjoy!";
+			$scope.recommendMessageRed=null;
 		}else{
-		$scope.recommendMessage="Too much!";
+			$scope.recommendMessageGreen="Too much!";
+			$scope.recommendMessageRed=null;
 		};
 		};
 		};
